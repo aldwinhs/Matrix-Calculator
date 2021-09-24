26bc126a3f83a6b70242230a0ee6f1d4fa7cd630
SurTan02 <suryanto.tan5@gmail.com>
2021-09-24 19:46:52 +0700

    input file belum

---
+++ mtrx/MainClass.java
@@ -0,0 +1,9 @@
+package mtrx;
+
+public class MainClass {
+    public static void main(String[] args){
+        matriks tes = new matriks();
+        tes.readMatrix();
+        tes.displayMatrix();
+    }
+}
