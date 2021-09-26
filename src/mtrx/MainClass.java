a337aac38727c86148c2cd206b8b0db73c653e17
Brianaldo <brianaldophandiarta31@gmail.com>
2021-09-26 17:52:29 +0700

    add test case

--- src/mtrx/MainClass.java
+++ src/mtrx/MainClass.java
@@ -5,22 +5,24 @@ public class MainClass {
         Matrix tes = new Matrix();
         tes.readMatrix();
         tes.displayMatrix();
-        Determinant detMatriks = new Determinant();
-        SPL tesGauss = new SPL();
+        // Determinant detMatriks = new Determinant();
+        // SPL tesGauss = new SPL();
 
 
-        System.out.print("----------------------------------\n");
-        System.out.print("coba buat matriks reduksi\n");
-        tesGauss.Gauss(tes).displayMatrix();
-        System.out.print("----------------------------------\n");
-        System.out.print("det reduksi baris : ");
-        System.out.print(detMatriks.detReduksiBaris(tes));
+        // System.out.print("----------------------------------\n");
+        // System.out.print("coba buat matriks reduksi\n");
+        // tesGauss.Gauss(tes).displayMatrix();
+        // System.out.print("----------------------------------\n");
+        // System.out.print("det reduksi baris : ");
+        // System.out.print(detMatriks.detReduksiBaris(tes));
         
-        System.out.print("\n----------------------------------\n");
-        System.out.print("det faktor : ");
-        System.out.print(detMatriks.detKofaktor(tes));
+        // System.out.print("\n----------------------------------\n");
+        // System.out.print("det faktor : ");
+        // System.out.print(detMatriks.detKofaktor(tes));
 
-        
+        Eliminasi eliminasi = new Eliminasi();
+        tes = eliminasi.metodeGauss(tes);
+        tes.displayMatrix();
     }
 }
 
