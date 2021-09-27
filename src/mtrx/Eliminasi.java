6e9e857e761ee4599a0f210ca624d29b0bf6c8bd
Brianaldo <brianaldophandiarta31@gmail.com>
2021-09-27 19:06:03 +0700

    fix metodeInverse

--- src/mtrx/Eliminasi.java
+++ src/mtrx/Eliminasi.java
@@ -87,11 +87,87 @@ public class Eliminasi {
 
 
     public Matrix metodeinverse(Matrix matrix){
-        Matrix matrixHasil = new Matrix(matrix);
-        int nRow = matrixHasil.getRowEff();
-        int nCol = matrixHasil.getColEff();
+        // Matrix matrixHasil = new Matrix(matrix);
+        // int nRow = matrixHasil.getRowEff();
+        // int nCol = matrixHasil.getColEff();
 
+        // matrixHasil.addIdentity();
+
+        /* KAMUS */
+        Matrix matrixHasil = new Matrix(matrix);
+        int[] lenght = new int[matrix.getRowEff()];
+        int i, j, k;
+        /* ALGORITMA */
         matrixHasil.addIdentity();
+
+        for (i = 0; i < matrixHasil.getRowEff(); i++) {
+            j = 0;
+            while (j < matrixHasil.getColEff() / 2 && matrixHasil.getElement(i, j) == 0) {
+                lenght[i] = lenght[i] + 1;
+                j++;
+            }
+        }
+ 
+        // One by one move boundary of unsorted subarray
+        for (i = 0; i < matrixHasil.getRowEff() - 1; i++)
+        {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (j = i + 1; j < matrixHasil.getRowEff(); j++)
+                if (lenght[j] < lenght[min_idx])
+                    min_idx = j;
+ 
+            // Swap the found minimum element with the first
+            // element
+            int temp = lenght[min_idx];
+            lenght[min_idx] = lenght[i];
+            lenght[i] = temp;
+
+            // Swap row
+            matrixHasil.swapRow(i, min_idx);
+        }
+        for (i = 0; i < matrixHasil.getRowEff(); i++) {
+            //System.out.println(lenght[i]);
+        }
+
+        for (i = 0; i < matrixHasil.getRowEff(); i++) {
+            j = 0;
+            while (j < i) {
+                if (lenght[i] == lenght[j]) {
+                    matrixHasil.plusMinusRow(i, j, 1, matrixHasil.getElement(i, lenght[i]), false);
+                    lenght[i] = lenght[i] + 1;
+                    k = lenght[i];
+                    while (k < matrixHasil.getColEff() / 2 && matrixHasil.getElement(i, k) == 0) {
+                        lenght[i] = lenght[i] + 1;
+                        k++;
+                    }
+                }
+                j++;
+            }
+            if (lenght[i] < matrixHasil.getColEff() / 2) matrixHasil.multiplyRow(i, 1/matrixHasil.getElement(i, lenght[i]));
+        }
+
+        boolean satuNotFound;
+
+        for (i = 1; i< matrixHasil.getRowEff(); i++){
+            j = 0;
+            satuNotFound = true;
+            while (satuNotFound && j<matrixHasil.getColEff() / 2) {
+                if (matrixHasil.getElement(i, j) == 1) {
+                    satuNotFound = false;
+                    for ( k = 0; k < i; k++) {
+                        matrixHasil.plusMinusRow(k, j, 1, matrixHasil.getElement(k, j), false);
+                    }
+                } else {
+                    j++;
+                }
+            }
+        }
+
+        for (i = 0; i < matrix.getColEff(); i++) {
+            matrixHasil.deleteCol(0);
+        }
+
         return matrixHasil;
     }
         
