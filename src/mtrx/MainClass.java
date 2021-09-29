15f13c7b827aab5b9ec48f67498fe13aac839917
SurTan02 <suryanto.tan5@gmail.com>
2021-09-29 15:59:10 +0700

    tambah beberapa tc

--- src/mtrx/MainClass.java
+++ src/mtrx/MainClass.java
@@ -1,6 +1,7 @@
 package mtrx;
 import java.util.Scanner;
 
+
 public class MainClass {
 
     public static void NewLine(){
@@ -33,7 +34,7 @@ public class MainClass {
         
         /* Ujicoba Eliminasi cara Gauss  dan Gauss Jordan dengan inputFile*/
         Eliminasi tesEliminasi = new Eliminasi();
-        tesEliminasi.getMatrixEselonBarisTereduksi(InputFile.matriksForm).displayMatrix();
+        //tesEliminasi.getMatrixEselonBarisTereduksi(InputFile.matriksForm).displayMatrix();
         NewLine();
         //tesEliminasi.getMatrixEselonBaris(InputFile.matriksForm).displayMatrix();
         NewLine();
@@ -42,21 +43,33 @@ public class MainClass {
 
         /*ujicoba SPL dengan 4 metode */
         SPL solusiFile = new SPL();
-        double[] x =solusiFile.BackwardSubstitution(tesEliminasi.getMatrixEselonBarisTereduksi(InputFile.matriksForm));
+        //double[] x =solusiFile.BackwardSubstitution(tesEliminasi.getMatrixEselonBarisTereduksi(InputFile.matriksForm));
         //double[] x =solusiFile.BackwardSubstitution(tesEliminasi.getMatrixEselonBaris(InputFile.matriksForm));
         //double[] x =solusiFile.cramerMethod(InputFile.matriksForm);
         //double[] x =solusiFile.inverseMethod(InputFile.matriksForm);
         NewLine();
-       
+
+       /*ujicoba interpolasi*/
+        Interpolasi SoalInter = new Interpolasi();
+        /*khusus interpolasi
+        Matrix matrixInter = new Matrix(InputFile.matriksForm);
+        matrixInter.deleteCol(matrixInter.getLastIdxRow());
+
+        */
+        double[] x = SoalInter.SolveInterpolasi(InputFile.matriksForm, InputFile.count);
         for (int i = 0; i<x.length; i++){
             System.out.print("solusi x"+ (i+1) + " : "+ x[i]) ;
             System.out.print("\n");
         }
-        
+        double[] taksir = new double[1];
+        taksir[0] = SoalInter.SolveTaksiran( x,9.2);
         fileName = scan.next();
         //Untuk WriteFile, ntar dicek sudah ada file dengan nama sama/belum
-        InputFile.writeMatrixFile(fileName, x);
+        //untuk solusi SPL
+        //InputFile.writeMatrixFile(fileName, x);
         
+        //untuk solusi determinan, taksiran
+        InputFile.writeDoubleFile(fileName, taksir);
         scan.close();
         
     }
