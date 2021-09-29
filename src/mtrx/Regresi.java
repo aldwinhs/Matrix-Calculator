4c33b2e59045e92c0effdbb01138533f751bfa85
Aldwin Hardi Swastia <ahardiswastia@gmail.com>
2021-09-29 17:39:35 +0700

    Update Regresi.java

--- src/mtrx/Regresi.java
+++ src/mtrx/Regresi.java
@@ -1,12 +1,8 @@
 package mtrx;
 
-import java.util.Scanner;
-
 public class Regresi {
     public Matrix NormalEstimation(Matrix nilai, int n, int m){
-        Scanner obj = new Scanner(System.in);
         int i, j, k; double sum;
-        obj.close();
         Matrix hasil = new Matrix(m+1, m+2);
         hasil.setElement(0, 0, n);
         for ( j = 1; j < hasil.getColEff(); j++) {
