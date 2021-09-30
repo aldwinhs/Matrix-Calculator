ceb4807bd3c1d43fbb4032a83681921b8e6aefd5
Brianaldo <brianaldophandiarta31@gmail.com>
2021-10-01 03:39:59 +0700

    fix getEselonBaris

--- src/mtrx/Eliminasi.java
+++ src/mtrx/Eliminasi.java
@@ -3,51 +3,97 @@ package mtrx;
 public class Eliminasi {
     
     /* INISIALISASI VARIABEL */
+    int[] lenght;
 
     /* KONSTRUKTOR */
 
     /* METHOD */
+    public Matrix sortMatrix(Matrix matrix) {
+         /* KAMUS */
+         Matrix matrixHasil = new Matrix(matrix);
+         lenght = new int[matrix.getRowEff()];
+         int i, j;
+         /* ALGORITMA */
+         for (i = 0; i < matrixHasil.getRowEff(); i++) {
+             j = 0;
+             lenght[i] = 0;
+             while (j < matrixHasil.getColEff() && matrixHasil.getElement(i, j) == 0) {
+                 lenght[i] = lenght[i] + 1;
+                 j++;
+             }
+         }
+  
+         // One by one move boundary of unsorted subarray
+         for (i = 0; i < matrixHasil.getRowEff() - 1; i++)
+         {
+             // Find the minimum element in unsorted array
+             int min_idx = i;
+             for (j = i + 1; j < matrixHasil.getRowEff(); j++)
+                 if (lenght[j] < lenght[min_idx])
+                     min_idx = j;
+  
+             // Swap the found minimum element with the first
+             // element
+             int temp = lenght[min_idx];
+             lenght[min_idx] = lenght[i];
+             lenght[i] = temp;
+ 
+             // Swap row
+             matrixHasil.swapRow(i, min_idx);
+         }
+
+         return matrixHasil;
+    }
+
     public Matrix getMatrixEselonBaris(Matrix matrix) {
         /* KAMUS */
         Matrix matrixHasil = new Matrix(matrix);
-        int[] lenght = new int[matrix.getRowEff()];
+        lenght = new int[matrix.getRowEff()];
         int i, j, k;
+        matrixHasil = new Matrix(sortMatrix(matrixHasil));
         /* ALGORITMA */
+        // for (i = 0; i < matrixHasil.getRowEff(); i++) {
+        //     j = 0;
+        //     while (j < matrixHasil.getColEff() && matrixHasil.getElement(i, j) == 0) {
+        //         lenght[i] = lenght[i] + 1;
+        //         j++;
+        //     }
+        // }
+ 
+        // // One by one move boundary of unsorted subarray
+        // for (i = 0; i < matrixHasil.getRowEff() - 1; i++)
+        // {
+        //     // Find the minimum element in unsorted array
+        //     int min_idx = i;
+        //     for (j = i + 1; j < matrixHasil.getRowEff(); j++)
+        //         if (lenght[j] < lenght[min_idx])
+        //             min_idx = j;
+ 
+        //     // Swap the found minimum element with the first
+        //     // element
+        //     int temp = lenght[min_idx];
+        //     lenght[min_idx] = lenght[i];
+        //     lenght[i] = temp;
+
+        //     // Swap row
+        //     matrixHasil.swapRow(i, min_idx);
+        // }
+        // // matrixHasil.displayMatrix();
+        // // for (i = 0; i < matrixHasil.getRowEff(); i++) {
+        // //     //System.out.println(lenght[i]);
+        // // }
+
         for (i = 0; i < matrixHasil.getRowEff(); i++) {
             j = 0;
-            while (j < matrixHasil.getColEff() && matrixHasil.getElement(i, j) == 0) {
-                lenght[i] = lenght[i] + 1;
-                j++;
-            }
-        }
- 
-        // One by one move boundary of unsorted subarray
-        for (i = 0; i < matrixHasil.getRowEff() - 1; i++)
-        {
-            // Find the minimum element in unsorted array
-            int min_idx = i;
-            for (j = i + 1; j < matrixHasil.getRowEff(); j++)
-                if (lenght[j] < lenght[min_idx])
-                    min_idx = j;
- 
-            // Swap the found minimum element with the first
-            // element
-            int temp = lenght[min_idx];
-            lenght[min_idx] = lenght[i];
-            lenght[i] = temp;
-
-            // Swap row
-            matrixHasil.swapRow(i, min_idx);
-        }
-        for (i = 0; i < matrixHasil.getRowEff(); i++) {
-            //System.out.println(lenght[i]);
-        }
-
-        for (i = 0; i < matrixHasil.getRowEff(); i++) {
-            j = 0;
+            // PROCEDURE SORT
+            // matrixHasil = sortMatrix(matrixHasil);
             while (j < i) {
+                // matrixHasil = new Matrix(sortMatrix(matrixHasil));
+                matrixHasil.displayMatrix();
+                System.out.println();
+                System.out.println(lenght[i] + ' ' + lenght[j]);
                 if (lenght[i] == lenght[j]) {
-                    matrixHasil.plusMinusRow(i, j, 1, matrixHasil.getElement(i, lenght[i]), false);
+                    if (lenght[i] < matrixHasil.getColEff()) matrixHasil.plusMinusRow(i, j, 1, matrixHasil.getElement(i, lenght[i]), false);
                     lenght[i] = lenght[i] + 1;
                     k = lenght[i];
                     while (k < matrixHasil.getColEff() && matrixHasil.getElement(i, k) == 0) {
@@ -55,9 +101,38 @@ public class Eliminasi {
                         k++;
                     }
                 }
+
                 j++;
             }
             if (lenght[i] < matrixHasil.getColEff()) matrixHasil.multiplyRow(i, 1/matrixHasil.getElement(i, lenght[i]));
+            // matrixHasil.displayMatrix();
+            // System.out.println();
+        }
+        matrixHasil = new Matrix(sortMatrix(matrixHasil));
+        for (i = 0; i < matrixHasil.getRowEff(); i++) {
+            j = 0;
+            // PROCEDURE SORT
+            // matrixHasil = sortMatrix(matrixHasil);
+            while (j < i) {
+                // matrixHasil = new Matrix(sortMatrix(matrixHasil));
+                matrixHasil.displayMatrix();
+                System.out.println();
+                System.out.println(lenght[i] + ' ' + lenght[j]);
+                if (lenght[i] == lenght[j]) {
+                    if (lenght[i] < matrixHasil.getColEff()) matrixHasil.plusMinusRow(i, j, 1, matrixHasil.getElement(i, lenght[i]), false);
+                    lenght[i] = lenght[i] + 1;
+                    k = lenght[i];
+                    while (k < matrixHasil.getColEff() && matrixHasil.getElement(i, k) == 0) {
+                        lenght[i] = lenght[i] + 1;
+                        k++;
+                    }
+                }
+                
+                j++;
+            }
+            if (lenght[i] < matrixHasil.getColEff()) matrixHasil.multiplyRow(i, 1/matrixHasil.getElement(i, lenght[i]));
+            // matrixHasil.displayMatrix();
+            // System.out.println();
         }
         return matrixHasil;
     }
